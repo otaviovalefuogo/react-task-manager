3a8a744491ac12c2fa64d5fc6f67a75c74d7df3c
otaviovalefuogo <otaviovalefuogo28@gmail.com>
2026-09-22 23:30:57 -0300

    feat: adiciona funcionalidade de remover tarefas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronsRightIcon } from "lucide-react";
+import { ChevronsRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks(props) {
   return (
@@ -13,6 +13,12 @@ function Tasks(props) {
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronsRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
